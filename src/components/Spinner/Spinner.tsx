c1f5ec6826bef666e039802c3dc6c4cfb94f66ab
Marius Reimer <reime005@gmail.com>
2020-11-26 00:59:15 +0100

    --

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -6,6 +6,7 @@ import Animated, {
   withTiming,
   interpolateNode,
   interpolate,
+  useAnimatedStyle,
 } from 'react-native-reanimated';
 import Svg, { Circle as C } from 'react-native-svg';
 import { useTheme } from 'styled-components';
@@ -15,14 +16,14 @@ const Circle = Animated.createAnimatedComponent(C);
 const radius = 10;
 
 const DISTANCE = 10;
-const SPEED_FACTOR = 0.05;
+const SPEED_FACTOR = 0.07;
 
 const MAX_SCALE = 1;
 const MIN_SCALE = 0.5;
 
 const initialPositions = {
-  first: radius,
-  second: radius * 3 + radius / 8,
+  first: radius + radius * 0.2,
+  second: radius * 3 + radius * 0.2,
 };
 
 interface Props {
@@ -38,34 +39,57 @@ export const Spinner = (props: Props) => {
   const frameRef = React.useRef<any>(null);
   const currTime = useSharedValue(0);
 
+  const firstRadius = useSharedValue(radius);
+  const secondRadius = useSharedValue(radius);
+
   const x = {
     first: useSharedValue(initialPositions.first),
     second: useSharedValue(initialPositions.second),
   };
 
   const firstProps = useAnimatedProps(() => {
+    const s = Math.sin(currTime.value);
+
+    x.first.value = interpolate(
+      s,
+      [-1, 1],
+      [initialPositions.first, initialPositions.second],
+    );
+
+    firstRadius.value = interpolate(
+      s,
+      [-1, 0, 1],
+      [radius * 0.8, radius * 1.1, radius * 0.8],
+    );
+
     return {
       cx: x.first.value,
+      r: firstRadius.value,
     };
   });
 
   const secondProps = useAnimatedProps(() => {
+    const s = Math.sin(currTime.value);
+
+    x.second.value = interpolate(
+      s,
+      [1, -1],
+      [initialPositions.first, initialPositions.second],
+    );
+
+    secondRadius.value = interpolate(
+      s,
+      [-1, 0, 1],
+      [radius * 1.1, radius * 0.6, radius * 1.1],
+    );
+
     return {
       cx: x.second.value,
+      r: secondRadius.value,
     };
   });
 
   const req = React.useCallback(() => {
-    x.first.value = interpolate(
-      Math.sin(currTime.value),
-      [-1, 1],
-      [initialPositions.first, initialPositions.second],
-    );
-    x.second.value = interpolate(
-      Math.sin(currTime.value),
-      [1, -1],
-      [initialPositions.first, initialPositions.second],
-    );
     // x.second.value = radius * 2 + radius * Math.sin(currTime.value - 1);
     currTime.value = currTime.value + SPEED_FACTOR;
 
@@ -84,7 +108,7 @@ export const Spinner = (props: Props) => {
       width={48}
       {...props}
       viewBox="0 0 48 24"
-      style={{ backgroundColor: 'grey', alignSelf: 'center' }}>
+      style={{ backgroundColor: 'transparent', alignSelf: 'center' }}>
       <Circle
         cy={radius + 2}
         r={radius}
